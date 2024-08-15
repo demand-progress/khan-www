821080fb260ef798899c150abc04ebcd38acaf75
@owise1 <owise1@gmail.com>
2024-08-15 11:19:11 -0400

    remove svg

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.scss'
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import {Carousel} from 'react-responsive-carousel'
